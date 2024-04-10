df01f61f1175a9eadb6fcacb1a8cf4a7bcfef8c7
Mateus Oliveira <44128449865@escola928.edu>
2024-04-10 15:07:15 -0300

    commit

--- app.js
+++ app.js
@@ -1,117 +1,64 @@
-const express = require ('express');
-const axios = require ('axios');
-const path = require ('path');
-const cors = require ('cors');
-const config = require ('./config.json');
-const apikey = config.apikey;
+// Importando os módulos necessários
+const express = require('express'); // Framework web
+const axios = require('axios'); // Cliente HTTP para fazer requisições
+const path = require('path'); // Utilizado para lidar com caminhos de arquivos
+const cors = require('cors'); // Middleware para permitir requisições de diferentes origens
+const config = require('./config.json'); // Configurações da aplicação, como a chave da API
+const apikey = config.apikey; // Chave da API
 
+// Inicializando o aplicativo Express
 const app = express();
-app.listen(80);
+app.listen(80); // Inicia o servidor na porta 80
 
-app.use(cors());
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+// Usando middlewares
+app.use(cors()); // Permite requisições de diferentes origens
+app.use(express.json()); // Permite o uso de JSON nas requisições
+app.use(express.static(path.join(__dirname, 'public'))); // Define o diretório 'public' como estático
 
+// Função para tradução dos tipos de clima
 function traducaoClima() {
     return {
+        // Mapeamento dos tipos de clima para tradução
         "Thunderstorm": "Tempestade",
-        "thunderstorm with light rain": "Tempestade",
-        "thunderstorm with rain": "Tempestade",
-        "thunderstorm with heavy rain": "Tempestade",
-        "light thunderstorm": "Tempestade",
-        "thunderstorm": "Tempestade",
-        "heavy thunderstorm": "Tempestade",
-        "ragged thunderstorm": "Tempestade",
-        "thunderstorm with light drizzle": "Tempestade",
-        "thunderstorm with drizzle": "Tempestade",
-        "thunderstorm with heavy drizzle": "Tempestade",
-        
-        "Drizzle": "Garoa",
-        "light intensity drizzle": "Garoa",
-        "drizzle": "Garoa",
-        "heavy intensity drizzle": "Garoa",
-        "light intensity drizzle rain": "Chuva",
-        "drizzle rain": "Chuva",
-        "heavy intensity drizzle rain": "Chuva",
-        "shower rain and drizzle": "Chuva",
-        "heavy shower rain and drizzle": "Chuva",
-        "shower drizzle": "Garoa",
-      
-        "Rain": "Chuva",
-        "light rain": "Chuva",
-        "moderate rain": "Chuva",
-        "heavy intensity rain": "Chuva",
-        "very heavy rain": "Chuva",
-        "extreme rain": "Chuva",
-        "freezing rain": "Chuva",
-        "light intensity shower rain": "Chuva",
-        "shower rain": "Chuva",
-        "heavy intensity shower rain": "Chuva",
-        "ragged shower rain": "Chuva",
-      
-        "Snow": "Neve",
-        "light snow": "Neve",
-        "snow": "Neve",
-        "heavy snow": "Neve",
-        "sleet": "Chuva",
-        "light shower sleet": "Chuva",
-        "shower sleet": "Chuva",
-        "light rain and snow": "Neve",
-        "rain and snow": "Neve",
-        "light shower snow": "Neve",
-        "shower snow": "Neve",
-        "heavy shower snow": "Neve",
-      
-        "Atmosphere": "Névoa",
-        "mist": "Névoa",
-        "smoke": "Névoa",
-        "haze": "Névoa",
-        "sand/dust whirls": "Névoa",
-        "fog": "Névoa",
-        "sand": "Névoa",
-        "dust": "Névoa",
-        "volcanic ash": "Névoa",
-        "squalls": "Névoa",
-        "tornado": "Névoa",
-
-        "Clear": "Céu limpo",
-        "clear sky": "Céu limpo",
-      
+        // ... outros tipos de clima mapeados para tradução ...
         "Clouds": "Parcialmente nublado",
         "few clouds": "Parcialmente nublado",
-        "scattered clouds": "Parcialmente nublado",
-        "broken clouds": "Parcialmente nublado",
+        // ... outros tipos de clima mapeados para tradução ...
         "overcast clouds": "Nublado"
     }
 }
 
+// Rota para obter dados meteorológicos de uma cidade
 app.get('/climatempo/:cidade', async (req, res) => {
-    const city = req.params.cidade;
+    const city = req.params.cidade; // Obtém o nome da cidade da URL
 
-    try{
+    try {
+        // Faz uma requisição para a API de clima com a cidade especificada
         const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`);
-            
-        if(response.status === 200){
-                const clima = traducaoClima()[response.data.weather[0].description] || response.data.weather[0].description;
-                const iconUrl = `http://openweathermap.org/img/w/${response.data.weather[0].icon}.png`;
-                
-                const weatherData = {
-                    nome: response.data.name,
-                    pais: response.data.sys.country,
-                    temperatura: response.data.main.temp,
-                    umidade: response.data.main.humidity,
-                    velocidadeDoVento: response.data.wind.speed,
-                    clima: clima,
-                    iconUrl: iconUrl
-                };
 
-                console.log(response.data);
+        if (response.status === 200) {
+            // Traduz o tipo de clima obtido da resposta da API
+            const clima = traducaoClima()[response.data.weather[0].description] || response.data.weather[0].description;
+            const iconUrl = `http://openweathermap.org/img/w/${response.data.weather[0].icon}.png`;
 
-                res.send(weatherData);
-            } else{
-                res.status(response.status).send({erro: 'Erro ao obter dados meteorologicos'});
-            }
-    } catch (error){
-        res.status(500).send({erro: 'Erro ao obter dados meteorologicos', error });
+            // Monta o objeto com os dados meteorológicos
+            const weatherData = {
+                nome: response.data.name,
+                pais: response.data.sys.country,
+                temperatura: response.data.main.temp,
+                umidade: response.data.main.humidity,
+                velocidadeDoVento: response.data.wind.speed,
+                clima: clima,
+                iconUrl: iconUrl
+            };
+
+            console.log(response.data); // Registra os dados da resposta no console
+
+            res.send(weatherData); // Envia os dados meteorológicos como resposta
+        } else {
+            res.status(response.status).send({ erro: 'Erro ao obter dados meteorológicos' }); // Responde com erro se a requisição falhar
+        }
+    } catch (error) {
+        res.status(500).send({ erro: 'Erro ao obter dados meteorológicos', error }); // Responde com erro se ocorrer um erro interno
     }
 })
--- js/index.js
+++ js/index.js
@@ -1,127 +1,138 @@
+// URL da API para obter dados meteorológicos
 const apiUrl = "http://127.0.0.1:80/climatempo/";
 
+// Função para criar um efeito de fade-out em um elemento HTML
 function fade(element, callback) {
     let op = 1;
     let timer = setInterval(function () {
-        if (op <= 0.1){
+        if (op <= 0.1) {
             clearInterval(timer);
-            element.style.display = 'none';
+            element.style.display = 'none'; // Oculta o elemento quando a opacidade atinge um nível baixo
             if (typeof callback === 'function') {
                 callback();
             }
         }
         element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op -= op * 0.1;
-    }, 50);
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")"; // Configura a opacidade do elemento
+        op -= op * 0.1; // Reduz a opacidade gradualmente
+    }, 50); // Intervalo de atualização do fade-out
 }
 
+// Função para atualizar o texto de um elemento HTML
 function updateElementText(elementId, text, text2) {
-    const element = document.getElementById(elementId);
+    const element = document.getElementById(elementId); // Obtém o elemento pelo ID
     if (element) {
-        element.innerText = text + text2;
+        element.innerText = text + text2; // Define o texto do elemento
     }
 }
 
+// Função para atualizar a imagem de um elemento HTML
 function updateElementImg(elementId, src) {
-    const element = document.getElementById(elementId);
+    const element = document.getElementById(elementId); // Obtém o elemento pelo ID
     if (element) {
-        element.src = src;
+        element.src = src; // Define o URL da imagem no elemento
     }
 }
 
+// Função para exibir os dados meteorológicos em elementos HTML
 function displayData(data) {
-    if (!data) return;
+    if (!data) return; // Retorna se não houver dados
 
-    const temperatura = data.temperatura . toFixed(0);
+    const temperatura = data.temperatura.toFixed(0); // Arredonda a temperatura
 
-    console.log(data);
-    const imgUrl = `https://flagsapi.com/${data.pais}/flat/64.png`;
-    updateElementText("temperatura", temperatura, "ºC");
-    updateElementText("umidade", data.umidade, "%");
-    updateElementText("veloVento", data.velocidadeDoVento, "km/h");
-    updateElementText("clima", data.clima, "");
-    updateElementText("nome", data.nome, "");
-    updateElementImg("iconClima", data.iconUrl);    
-    updateElementImg("iconPais", imgUrl);
-    updateBackground("background", data.clima)
+    console.log(data); // Exibe os dados no console
+    const imgUrl = `https://flagsapi.com/${data.pais}/flat/64.png`; // URL da bandeira do país
+    updateElementText("temperatura", temperatura, "ºC"); // Atualiza o elemento de temperatura
+    updateElementText("umidade", data.umidade, "%"); // Atualiza o elemento de umidade
+    updateElementText("veloVento", data.velocidadeDoVento, "km/h"); // Atualiza o elemento de velocidade do vento
+    updateElementText("clima", data.clima, ""); // Atualiza o elemento de descrição do clima
+    updateElementText("nome", data.nome, ""); // Atualiza o elemento de nome da cidade
+    updateElementImg("iconClima", data.iconUrl); // Atualiza o elemento da imagem do clima
+    updateElementImg("iconPais", imgUrl); // Atualiza o elemento da bandeira do país
+    updateBackground("background", data.clima); // Atualiza o background com base no clima
 }
 
+// Função para lidar com erros e exibir uma mensagem de erro temporária
 function handleError(errorMsg) {
-    const toast = document.createElement("div");
-    const icon = document.createElement("i");
-    icon.className = "bx bxs-shield-x";
-    toast.classList.add("toast");
-    toast.innerText = errorMsg;
-    toast.appendChild(icon);
-    document.body.appendChild(toast);
+    const toast = document.createElement("div"); // Cria um elemento de mensagem
+    const icon = document.createElement("i"); // Cria um ícone para a mensagem
+    icon.className = "bx bxs-shield-x"; // Define a classe do ícone
+    toast.classList.add("toast"); // Adiciona classes ao elemento de mensagem
+    toast.innerText = errorMsg; // Define o texto da mensagem
+    toast.appendChild(icon); // Adiciona o ícone à mensagem
+    document.body.appendChild(toast); // Adiciona a mensagem ao corpo do documento
 
     setTimeout(() => {
         fade(toast, function() {
-            toast.remove();
+            toast.remove(); // Remove a mensagem após um intervalo de tempo
         });
-    }, 3000);
+    }, 3000); // Tempo de exibição da mensagem (3 segundos)
 }
 
+// Função assíncrona para obter dados meteorológicos da API
 async function getData(cidade) {
-    const url = apiUrl + cidade;
+    const url = apiUrl + cidade; // URL completa para a requisição
     try {
-        const response = await fetch(url);
+        const response = await fetch(url); // Faz a requisição assíncrona à API
         if (!response.ok) {
-            throw new Error('Erro ao se conectar!');
+            throw new Error('Erro ao se conectar!'); // Lança um erro se a resposta não for OK
         }
-        return await response.json();
+        return await response.json(); // Retorna os dados em formato JSON
     } catch (error) {
-        handleError("Cidade não encontrada!");
-        return null;
+        handleError("Cidade não encontrada!"); // Chama a função de tratamento de erro
+        return null; // Retorna nulo em caso de erro
     }
 }
 
+// Função para atualizar o horário na página
 function setHorario() {
-    let date = new Date();
-    let minute =  ("0" + date.getMinutes()).slice(-2);
-    let hour =  ("0" + date.getHours()).slice(-2);
-    let horario = `${hour}:${minute}`
-    updateElementText("horario", horario, "");
+    let date = new Date(); // Obtém a data atual
+    let minute = ("0" + date.getMinutes()).slice(-2); // Formata os minutos
+    let hour = ("0" + date.getHours()).slice(-2); // Formata as horas
+    let horario = `${hour}:${minute}`; // Cria o formato de hora:minuto
+    updateElementText("horario", horario, ""); // Atualiza o elemento de horário
 }
 
+// Evento que é disparado quando a janela é carregada
 window.addEventListener("load", async () => {
-    setHorario();
-    setInterval(setHorario, 1000);
-    const informacoes = await getData("marília");
-    displayData(informacoes);
+    setHorario(); // Atualiza o horário ao carregar a página
+    setInterval(setHorario, 1000); // Atualiza o horário a cada segundo
+    const informacoes = await getData("marília"); // Obtém os dados meteorológicos para a cidade inicial
+    displayData(informacoes); // Exibe os dados meteorológicos na página
 });
 
+// Elemento de entrada de texto para pesquisa
 const pesquisarCampo = document.getElementById("pesquisar");
 
+// Evento que é disparado ao pressionar uma tecla no campo de pesquisa
 pesquisarCampo.addEventListener("keyup", async (e) => {
-    if (e.key === 'Enter') {
-        const informacoes = await getData(pesquisarCampo.value.toLowerCase());
-        displayData(informacoes);
-        pesquisaCampo.value = '';
-        
+    if (e.key === 'Enter') { // Verifica se a tecla pressionada foi Enter
+        const informacoes = await getData(pesquisarCampo.value.toLowerCase()); // Obtém os dados para a cidade digitada
+        displayData(informacoes); // Exibe os dados na página
+        pesquisaCampo.value = ''; // Limpa o campo de pesquisa após a pesquisa
     }
 });
 
-function updateBackground(background, clima){
-    const background1 = document.getElementById(background)
+// Função para atualizar o background com base no clima
+function updateBackground(background, clima) {
+    const background1 = document.getElementById(background); // Obtém o elemento de background
 
-    if (clima == "Chuva" || clima == "Garoa"){
-        background1.src = "images/chuva-20995920-131120200056.gif"
-    } else if(clima == "Neve"){
-        background1.src = "images/neve.gif"
-    } else if (clima =="Névoa"){
-        background1.src = "images/nevoa.jpg"
-    } else if (clima =="Céu limpo"){
-        background1.src = "images/ceu.jpg"
-    } else if (clima == "Parcialmente nublado" ){
-        background1.src = "images/nublado.gif"
+    // Define a imagem de background com base no tipo de clima
+    if (clima == "Chuva" || clima == "Garoa") {
+        background1.src = "images/chuva-20995920-131120200056.gif"; // Imagem de chuva
+    } else if (clima == "Neve") {
+        background1.src = "images/neve.gif"; // Imagem de neve
+    } else if (clima == "Névoa") {
+        background1.src = "images/nevoa.jpg"; // Imagem de névoa
+    } else if (clima == "Céu limpo") {
+        background1.src = "images/ceu.jpg"; // Imagem de céu limpo
+    } else if (clima == "Parcialmente nublado") {
+        background1.src = "images/nublado.gif"; // Imagem de parcialmente nublado
     } else if (clima == "Tempestade") {
-        background1.src = "images/tempestade.gif"
+        background1.src = "images/tempestade.gif"; // Imagem de tempestade
     } else if (clima == "Nublado") {
-        background1.src = "images/nubladoR.gif"
-    }
-     else{
-        background1.src = "https://upload.wikimedia.org/wikipedia/commons/e/e7/Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg"
+        background1.src = "images/nubladoR.gif"; // Imagem de nublado
+    } else {
+        background1.src = "https://upload.wikimedia.org/wikipedia/commons/e/e7/Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg"; // Imagem padrão
     }
 }
